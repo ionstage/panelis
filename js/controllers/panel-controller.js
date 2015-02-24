ff9aa2980349150e604d8e479cd486f170b8b9b3
nasitra <nasitra@users.noreply.github.com>
2015-02-24 17:18:35 +0900

    Fix style issue

--- js/controllers/panel-controller.js
+++ js/controllers/panel-controller.js
@@ -4,10 +4,10 @@
   var m = global.m || require('mithril');
 
   var PanelController = function(option) {
-    this.panel = m.prop(option.panel || null),
-    this.x = m.prop(option.x || 0), 
-    this.y = m.prop(option.y || 0),
-    this.width = m.prop(option.width || 72)
+    this.panel = m.prop(option.panel || null);
+    this.x = m.prop(option.x || 0);
+    this.y = m.prop(option.y || 0);
+    this.width = m.prop(option.width || 72);
   };
 
   app.PanelController = PanelController;

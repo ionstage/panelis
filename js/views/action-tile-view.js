4e2af22cb736f203a3732d503009de08fe5f77fb
nasitra <nasitra@users.noreply.github.com>
2015-03-01 10:49:21 +0900

    Refactor action-tile-view

--- js/views/action-tile-view.js
+++ js/views/action-tile-view.js
@@ -52,15 +52,11 @@
       config: function(element, isInitialized) {
         if (isInitialized)
           return;
+
         var panelElement = element.childNodes[0];
         addClass(panelElement, 'stop');
-
-        // rotation end
         panelElement.addEventListener('transitionend', function() {
-          ctrl.dispatchEvent({
-            type: 'rotationend'
-          });
-          replaceClass(panelElement, 'rotate', 'stop');
+          rotationEndPanel(ctrl, panelElement);
         });
 
         oninitialize({element: panelElement});
@@ -90,31 +86,13 @@
       width: panelWidth * colLength,
       height: panelWidth * rowLength,
       startHandler: function(event) {
-        // rotate
-        if (selectedPanel && selectedPosition) {
-          if (app.view.supportsTransitionEnd) {
-            replaceClass(panelElementProp(), 'stop', 'rotate');
-          } else {
-            ctrl.dispatchEvent({
-              type: 'rotationend'
-            });
-            replaceClass(panelElementProp(), 'rotate', 'stop');
-          }
-          return;
-        }
+        var x = event.clientX || event.touches[0].clientX;
+        var y = event.clientY || event.touches[0].clientY;
 
-        // select
-        var loc = app.view.globalToLocal({
-          x: event.clientX || event.touches[0].clientX,
-          y: event.clientY || event.touches[0].clientY
-        });
-        var row = parseInt((loc.y + panelWidth * rowLength / 2) / panelWidth, 10);
-        var col = parseInt((loc.x + panelWidth * colLength / 2) / panelWidth, 10);
-        ctrl.dispatchEvent({
-          type: 'select',
-          row: row,
-          col: col
-        });
+        if (selectedPanel && selectedPosition)
+          rotatePanel(ctrl, panelElementProp());
+        else
+          selectPanel(ctrl, x, y);
       },
       config: function(element, isInitialized) {
         if (isInitialized)
@@ -122,6 +100,37 @@
         var eventName = app.view.supportsTouch ? 'touchstart' : 'mousedown';
         element.addEventListener(eventName, this.attrs.startHandler);
       }
+    });
+  };
+
+  var rotatePanel = function(ctrl, panelElement) {
+    if (app.view.supportsTransitionEnd)
+      replaceClass(panelElement, 'stop', 'rotate');
+    else
+      rotationEndPanel(ctrl, panelElement);
+  };
+
+  var rotationEndPanel = function(ctrl, panelElement) {
+    ctrl.dispatchEvent({
+      type: 'rotationend'
+    });
+    replaceClass(panelElement, 'rotate', 'stop');
+  };
+
+  var selectPanel = function(ctrl, x, y) {
+    var tile = ctrl.tile();
+    var panelWidth = ctrl.panelWidth();
+
+    var rowLength = tile.rowLength();
+    var colLength = tile.colLength();
+    var loc = app.view.globalToLocal({x: x, y: y});
+    var row = parseInt((loc.y + panelWidth * rowLength / 2) / panelWidth, 10);
+    var col = parseInt((loc.x + panelWidth * colLength / 2) / panelWidth, 10);
+
+    ctrl.dispatchEvent({
+      type: 'select',
+      row: row,
+      col: col
     });
   };
 

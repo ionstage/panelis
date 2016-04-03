d0009eb158179d6e7b6fd8b159200bbc49f2e675
nasitra <nasitra@users.noreply.github.com>
2016-04-03 11:55:34 +0900

    Add method to access the panel of the controller-component

--- js/components/controller-component.js
+++ js/components/controller-component.js
@@ -2,13 +2,39 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
   var ControllerComponent = helper.inherits(function(props) {
     ControllerComponent.super_.call(this);
 
+    this.panels = this.prop(new Array(3));
     this.element = this.prop(props.element);
   }, Component);
+
+  ControllerComponent.prototype.slotElement = function(index) {
+    return dom.child(this.element(), 2, 0, index);
+  };
+
+  ControllerComponent.prototype.panel = function(index, panel) {
+    var panels = this.panels();
+
+    if (typeof panel === 'undefined')
+      return panels[index];
+
+    var currentPanel = panels[index];
+
+    // remove current panel
+    if (currentPanel)
+      currentPanel.parentElement(null);
+
+    if (panel) {
+      // add new panel
+      panel.parentElement(this.slotElement(index));
+    }
+
+    panels[index] = panel;
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ControllerComponent;

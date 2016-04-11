8f0d5785d140fbab85f04ed292c7fce8f3d0a81a
nasitra <nasitra@users.noreply.github.com>
2016-04-11 18:46:18 +0900

    Add property to set the visibility of the root element at startup

--- js/components/root.js
+++ js/components/root.js
@@ -9,15 +9,23 @@
     Root.super_.call(this);
 
     this.fontSize = this.prop(props.fontSize);
+    this.visible = this.prop(false);
     this.element = this.prop(props.element);
 
     this.markDirty();
   }, Component);
 
   Root.prototype.redraw = function() {
-    dom.css(this.element(), {
+    var element = this.element();
+
+    dom.css(element, {
       fontSize: this.fontSize() + 'px'
     });
+
+    if (!this.visible()) {
+      dom.removeClass(element, 'hide');
+      this.visible(true);
+    }
   };
 
   if (typeof module !== 'undefined' && module.exports)

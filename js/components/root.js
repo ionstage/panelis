bbe2dc38d4f936844e027fdb07bc4ae8c4020a9a
nasitra <nasitra@users.noreply.github.com>
2016-04-11 18:27:30 +0900

    Enable changing the font size of the root element

--- js/components/root.js
+++ js/components/root.js
@@ -2,11 +2,23 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var Root = helper.inherits(function() {
+  var Root = helper.inherits(function(props) {
     Root.super_.call(this);
+
+    this.fontSize = this.prop(props.fontSize);
+    this.element = this.prop(props.element);
+
+    this.markDirty();
   }, Component);
+
+  Root.prototype.redraw = function() {
+    dom.css(this.element(), {
+      fontSize: this.fontSize() + 'px'
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = Root;

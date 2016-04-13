490e11a95a26658dcc5c4f989cba7f3038a616c1
nasitra <nasitra@users.noreply.github.com>
2016-04-13 18:44:31 +0900

    Add controller-relation for the relation of two controllers

---
+++ js/relations/controller-relation.js
@@ -0,0 +1,26 @@
+(function(app) {
+  'use strict';
+
+  var helper = app.helper || require('../helper.js');
+  var Relation = app.Relation || require('./relation.js');
+
+  var ControllerRelation = helper.inherits(function(props) {
+    this.whiteController = this.prop(props.whiteController);
+    this.blackController = this.prop(props.blackController);
+  }, Relation);
+
+  ControllerRelation.prototype.update = function(changedComponent) {
+    var whiteController = this.whiteController();
+    var blackController = this.blackController();
+
+    if (changedComponent === whiteController)
+      blackController.disabled(!whiteController.disabled());
+    else if (changedComponent === blackController)
+      whiteController.disabled(!blackController.disabled());
+  };
+
+  if (typeof module !== 'undefined' && module.exports)
+    module.exports = ControllerRelation;
+  else
+    app.ControllerRelation = ControllerRelation;
+})(this.app || (this.app = {}));
